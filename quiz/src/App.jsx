1537f9b6fb655e2b5727918c890732bc7229e62c
CaioBertoldo <caio23bezerra@gmail.com>
2025-01-28 13:51:44 -0400

    conclusão das principais funcionalidades

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 import Welcome from './components/Welcome/Welcome'
+import { useContext, useEffect } from 'react'
+import Question from './components/Question/Question'
+import { QuizContext } from './context/quiz'
+import GameOver from './components/GameOver/GameOver'
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
 
+  useEffect(() => {
+    dispatch({type: "REORDER_QUESTIONS"})
+  }, [])
 
   return (
     <>
       <div className="App">
         <h1>Quiz de Programação</h1>
-        <Welcome />
+        {quizState.gameStage === "Start" && <Welcome />}
+        {quizState.gameStage === "Playing" && <Question />}
+        {quizState.gameStage === "End" && <GameOver />}
       </div>
     </>
   )
